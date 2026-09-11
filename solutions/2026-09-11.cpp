2ddbef0e73768e74f4bd529c06346fbebef88e1b
usedchang <24211860219@stu.wzu.edu.cn>
2026-09-11 11:24:13 +0800

    refactor(2026-09-11): 题解改为 O(n) 双指针实现并补充题解推导
    - 移除前缀/后缀 min-cost 的 O(n*5) DP 与注释掉的 DSU 模板
    - 改为预处理 u/su 后缀位置，枚举 b 直接 O(n) 求 f(p)=p4+p5-p1-p2-6
    - 新增 solutions/2026-09-11.md：交换代价 → 中位数定理 → f(p) 的最小化推导

--- solutions/2026-09-11.cpp
+++ solutions/2026-09-11.cpp
@@ -2,55 +2,29 @@
 using namespace std;
 #define endl '\n'
 typedef long long ll;
-const ll INF=1e9;
-// struct DSU{
-//     vector<int>f;
-//     DSU(int n){
-//         f.resize(n+1);
-//         iota(f.begin(),f.end(),0);
-//     }
-//     int find(int x){return x==f[x]?x:f[x]=find(f[x]);}
-//     bool merge(int x,int y){
-//         int fx=find(x),fy=find(y);
-//         if(fx==fy) return false;
-//         f[fx]=fy;
-//         return true;
-//     }
-// };
+const ll INF=1e18;
 int main(){
     cin.tie(0)->ios::sync_with_stdio(false);
     int n;string s;
     cin>>n>>s;
     s="&"+s;
     string t="&spbsu";
-    vector<vector<ll>>p(n+2,vector<ll>(7,INF));
-    vector<vector<ll>>pre(n+2,vector<ll>(7,INF));
-    vector<vector<ll>>suf(n+2,vector<ll>(7,INF));
-    for(int i=1;i<=n;i++)  for(int j=1;j<=5;j++) if(s[i]==t[j]) p[i][j]=0;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=5;j++) pre[i][j]=min(p[i][j],pre[i-1][j]+1);
-    }
+    vector<int>u(n+2,2*n+1);
+    vector<int>su(n+2,2*n+1);
     for(int i=n;i>=1;i--){
-        for(int j=1;j<=5;j++) suf[i][j]=min(p[i][j],suf[i+1][j]+1);
+        if(s[i]=='u') u[i]=min(i,u[i+1]);
+        else u[i]=u[i+1];
+        if(s[i]=='s'&&u[i+1]!=2*n+1) su[i]=i+u[i+1];
+        else su[i]=su[i+1];
     }
-    /*
-    sbpsu
-    */
+    ll S=0,SP=0;
     ll ans=INF;
-    auto f=[&](int l,int r) ->ll {
-        ll s=INF;
-        ll suml=0;
-        for(int i=l;i<=r+1;i++){
-            ll sumr=0;
-            for(int j=i;j<=r;j++) sumr+=suf[j][j-l+1];
-            s=min(s,suml+sumr);
-            suml+=pre[i][i-l+1];
+    for(int i=1;i<=n;i++){
+        if(s[i]=='s') S=i;
+        else if(S!=0&&s[i]=='p') SP=S+i;
+        if(SP!=0&&su[i]!=2*n+1&&s[i]=='b') {
+            ans=min(ans,-SP+su[i]-6LL);
         }
-        return s;
-    };
-    for(int l=1;l+5-1<=n;l++){
-        int r=l+5-1;
-        ans=min(ans,f(l,r));
     }
     cout<<ans<<endl;
     return 0;
