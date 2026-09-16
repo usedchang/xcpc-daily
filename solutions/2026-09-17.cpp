54a81f673547475446b7605ccf3586207e82eaec
usedchang <24211860219@stu.wzu.edu.cn>
2026-09-16 23:50:55 +0800

    feat(2026-09-17): 新增 CF 1965A Everything Nim 题解与代码

---
+++ solutions/2026-09-17.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+typedef long long ll;
+const ll inf=1e18;
+struct node{
+    int x,y,z;
+};
+void solve(){
+    int n;cin>>n;
+    vector<ll>a(n+1);
+    for(int i=1;i<=n;i++) cin>>a[i];
+    sort(a.begin()+1,a.end());
+    a.erase(unique(a.begin()+1,a.end()),a.end());
+    n=a.size()-1;
+    int op=1;
+    for(int i=1;i<=n;i++){
+        if(a[i]-a[i-1]!=1){
+            if(op){
+                cout<<"Alice"<<endl;
+                return;
+            }
+            else{
+                cout<<"Bob"<<endl;
+                return;
+            }
+        }
+        op^=1;
+    }
+    if(op) cout<<"Bob"<<endl;
+    else cout<<"Alice"<<endl;
+}
+int main(){
+    cin.tie(0)->ios::sync_with_stdio(false);
+    int T=1;cin>>T;
+    while(T--) solve();
+    return 0;
+}
